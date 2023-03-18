99247abd559941e7bea2c64bff8c179d4c48f239
nvmt1999 <104155586+nvmt1999@users.noreply.github.com>
2023-03-19 00:23:52 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ function toggleTicketModel() {
     const modelSelect = document.querySelector(".ticket-surrounding");
     modelSelect.classList.toggle("hidden");
 }
+
+const modelContainerSelect = document.querySelector(".ticket-model");
+modelContainerSelect.addEventListener('click', (event) => event.stopPropagation());
+
+function displayNav() {
+    const navSelect = document.querySelector(".toggle-items");
+    navSelect.classList.toggle("hidden");
+}
