59c9d6f466332c25aef0ac720e36c6d850f64bae
nvmt1999 <104155586+nvmt1999@users.noreply.github.com>
2023-03-20 23:24:56 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Ticket Model
+
 function toggleTicketModel() {
     const modelSelect = document.querySelector(".ticket-surrounding");
     modelSelect.classList.toggle("hidden");
@@ -5,6 +7,8 @@ function toggleTicketModel() {
 
 const modelContainerSelect = document.querySelector(".ticket-model");
 modelContainerSelect.addEventListener('click', (event) => event.stopPropagation());
+
+//Navigation Model
 
 function displayNav() {
     const navSelect = document.querySelector(".toggle-items");
@@ -14,4 +18,21 @@ function displayNav() {
 function afterClick() {
     const navSelect = document.querySelector(".toggle-items");
     navSelect.classList.add("hidden");
+}
+
+//Image Slide
+
+let slideIndex = 0;
+showSlide();
+function showSlide() {
+    let slides = document.getElementsByClassName("slider");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlide, 1500);
 }
